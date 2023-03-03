f2840973a4215c343240b8f9f10639eb44cebc61
Junaid <info@junaidkhan.dev>
2023-03-03 17:03:44 +0000

    navbar done

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,36 @@
 import { Inter } from "@next/font/google"
 
-import styles from "./page.module.css"
-
 const inter = Inter({ subsets: ["latin"] })
 
 export default function Home(Props: any) {
   return (
-    <main className={styles.main}>
-      <h1>Next 13</h1>
+    <main className="bg-gray-100 min-h-screen w-screen">
+      <main className="max-w-screen-2xl m-auto bg-white">
+        {/* NAVBAR */}
+        <nav className="bg-white p-2 flex justify-between">
+          <a href="" className="font-bold text-gray-700 text-2xl">
+            OpenTable
+          </a>
+          <div>
+            <div className="flex">
+              <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
+                Signin
+              </button>
+              <button className="border p-1 px-4 rounded">Signup</button>
+            </div>
+          </div>
+        </nav>
+        {/* NAVBAR */}
+        {/* Header */}
+        <div className="h-64 p-2 bg-gradient-to-r from-[#0f1f46] to-[#5f6984]">
+          <div className="text-center mt-10">
+            <h1 className="text-white text-5xl font-bold mb-2">
+              Find your table for any occasion
+            </h1>
+          </div>
+        </div>
+        {/* Header */}
+      </main>
     </main>
   )
 }
