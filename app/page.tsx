f0ed4505ddbb4c2983e3a8197d1d34f3447879ce
Junaid <info@junaidkhan.dev>
2023-03-06 10:14:42 +0000

    pages added

--- app/page.tsx
+++ app/page.tsx
@@ -20,16 +20,55 @@ export default function Home(Props: any) {
             </div>
           </div>
         </nav>
-        {/* NAVBAR */}
-        {/* Header */}
-        <div className="h-64 p-2 bg-gradient-to-r from-[#0f1f46] to-[#5f6984]">
-          <div className="text-center mt-10">
-            <h1 className="text-white text-5xl font-bold mb-2">
-              Find your table for any occasion
+        <header className="h-96 overflow-hidden">
+          <div className="bg-center bg-gradient-to-r from-[#0f1f46] to-[#5f6984] h-full flex justify-center items-center">
+            <h1 className="text-7xl text-white capitalize text-shadow text-center">
+              Milestone Fast Food (LONDON)
             </h1>
           </div>
-        </div>
-        {/* Header */}
+        </header>
+        {/* description area */}
+        <section className="flex m-auto justify-between w-2/3 items-start 0 -mt-11">
+          <div className="bg-white w-[70%] rounded p-3 shadow">
+            {/* restaurant navbar */}
+            <nav className="text-reg flex border-b pb-2">
+              <a href="" className="mr-7">
+                Overview
+              </a>
+              <a href="" className="mr-7">
+                Menu
+              </a>
+            </nav>
+            {/* restaurant navbar */}
+            {/* title */}
+            <div className="mt-4 border-b pb-6">
+              <h1 className="font-bold text-6xl">Milestone Fast Food</h1>
+            </div>
+            {/* title */}
+            {/* Rating */}
+            <div className="flex items-end">
+              <div className="rating mt-2 flex items-center">
+                <p>****</p>
+                <p className="text-reg ml-3">4.8</p>
+              </div>
+              <div>
+                <p className="text-reg ml-4">600 Reviews</p>
+              </div>
+            </div>
+            {/* Rating */}
+            {/* description */}
+            <div className="mt-4">
+              <p className="text-l font-light">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+                laboriosam maiores cum atque obcaecati pariatur totam rem
+                mollitia ut tempora. Commodi enim molestiae hic architecto autem
+                consectetur blanditiis, minus aut.
+              </p>
+            </div>
+            {/* description */}
+          </div>
+        </section>
+        {/* description area */}
       </main>
     </main>
   )
